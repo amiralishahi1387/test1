b45fae90e6f2f41e4e680a82a8f16cc683556783
Amirali shahi <amiralishahi0675@gmail.com>
2025-02-14 06:57:15 -0300

    Add files via upload

---
+++ حل تمرین شماره 1.py
@@ -0,0 +1,22 @@
+num = int(input("adad khod ra vared konid : "))
+
+if 0 <= num <= 6:
+    print("🌚bo ro bekhab")
+elif 6< num <=12:
+    print("🌝sobhe shoma bekher")
+elif 12< num <=18:
+    print("☕🧇bo ro ye esterahat kon")
+elif 18< num <=24:
+    print("shab be khere")
+elif 24< num >=24:
+    print("🙏🌸zaman ra do rost vared kon")
+
+
+
+
+
+
+
+
+
+
